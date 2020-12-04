805ce2984e0f37ecff42ec0743b3da11a7d06c8d
Alexander Uskov <neolord.tmb@gmail.com>
2020-12-04 19:42:24 +0300

    add comma to game stub

--- src/cli.js
+++ src/cli.js
@@ -3,5 +3,5 @@ import readlineSync from 'readline-sync';
 export default async () => {
   console.log('Welcome to the Brain Games!');
   const name = await readlineSync.question('May I have your name, please: ');
-  console.log(`Hello ${name}!`);
+  console.log(`Hello, ${name}!`);
 };
